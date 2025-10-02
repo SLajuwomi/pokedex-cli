2ff0111830a1b062a856b661f8e0c5673f220f4b
SLajuwomi <stephenlajuwomi@gmail.com>
2025-10-02 09:42:38 -0500

    remove build files

--- main.go
+++ main.go
@@ -7,6 +7,40 @@ import (
 	"strings"
 )
 
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+var supportedCommands = map[string]cliCommand{
+	"exit": {
+		name:        "exit",
+		description: "Exit the Pokedex",
+		callback:    commandExit,
+	},
+	"help": {
+		name:        "help",
+		description: "Display the help menu",
+		callback:    help,
+	},
+}
+
+func commandExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
+}
+
+func help() error {
+	fmt.Println("Welcome to the Pokedex!")
+	fmt.Println("Usage:\n")
+	for k, v := range supportedCommands {
+		fmt.Printf("%s: %s\n", k, v.description)
+	}
+	return nil
+}
+
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 	for {
@@ -14,13 +48,18 @@ func main() {
 		scanner.Scan()
 		userString := scanner.Text()
 		userStringSlice := cleanInput(userString)
-		fmt.Printf("Your command was: %s\n", userStringSlice[0])
+		_, ok := supportedCommands[userStringSlice[0]]
+		if ok {
+			supportedCommands[userStringSlice[0]].callback()
+		} else {
+			fmt.Println("Unknown command")
+		}
 	}
+
 }
 
 func cleanInput(text string) []string {
 	cleaned_text := strings.ToLower(strings.TrimSpace(text))
 	final_slice := strings.Split(cleaned_text, " ")
-	return final_slice 
+	return final_slice
 }
-
