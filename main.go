6d6a2f67453eb8f2fe4c3244021de1e81d2e43e9
SLajuwomi <stephenlajuwomi@gmail.com>
2025-10-01 20:00:35 -0500

    implemented cleanInput function

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+func cleanInput(text string) []string {
+	cleaned_text := strings.ToLower(strings.TrimSpace(text))
+	final_slice := strings.Split(cleaned_text, " ")
+	fmt.Println(final_slice)
+	return final_slice 
+}
+
---
+++ repl_test.go
@@ -0,0 +1,36 @@
+package main
+
+import "testing"
+
+func TestCleanInput(t *testing.T) {
+	cases := []struct {
+		input string
+		expected []string
+	}{
+		{
+			input: "  hello world  ",
+			expected: []string{"hello", "world"},
+		},
+		{
+			input: "  Charmander Bulbasaur PIKACHU ",
+			expected: []string{"charmander", "bulbasaur", "pikachu"},
+		},
+	}
+
+	for _, c := range cases {
+		actual := cleanInput(c.input)
+
+		if len(actual) != len(c.expected) {
+			t.Errorf("lengths are not the same. expected: %d, but got %d", len(c.expected), len(actual))
+			t.Fail()
+		}
+		for i := range actual {
+			word := actual[i]
+			expectedWord := c.expected[i]
+			if word != expectedWord {
+				t.Errorf("words are not the same. expected: %s, but got %s", expectedWord, word)
+				t.Fail()
+			}
+		}
+	}
+}
