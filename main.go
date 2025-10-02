a3308d35884e4593cc571b7823f91551772277b5
SLajuwomi <stephenlajuwomi@gmail.com>
2025-10-02 09:06:50 -0500

    created repl

--- main.go
+++ main.go
@@ -1,18 +1,26 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 	"strings"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+		userString := scanner.Text()
+		userStringSlice := cleanInput(userString)
+		fmt.Printf("Your command was: %s\n", userStringSlice[0])
+	}
 }
 
 func cleanInput(text string) []string {
 	cleaned_text := strings.ToLower(strings.TrimSpace(text))
 	final_slice := strings.Split(cleaned_text, " ")
-	fmt.Println(final_slice)
 	return final_slice 
 }
 
