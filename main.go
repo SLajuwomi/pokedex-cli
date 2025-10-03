2df0529fef651190e1886da8823583fc40207ee8
SLajuwomi <stephenlajuwomi@gmail.com>
2025-10-03 10:37:51 -0500

    map is now working. displays the next 20 pokedex locations

--- main.go
+++ main.go
@@ -2,7 +2,11 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
 	"fmt"
+	"io"
+	"log"
+	"net/http"
 	"os"
 	"strings"
 )
@@ -10,39 +14,90 @@ import (
 type cliCommand struct {
 	name        string
 	description string
-	callback    func() error
+	callback    func(*config) error
 }
 
-var supportedCommands = map[string]cliCommand{
-	"exit": {
-		name:        "exit",
-		description: "Exit the Pokedex",
-		callback:    commandExit,
-	},
-	"help": {
-		name:        "help",
-		description: "Display the help menu",
-		callback:    help,
-	},
+type locationAreaList struct {
+	Count    int    `json:"count"`
+	Next     string `json:"next"`
+	Previous any    `json:"previous"`
+	Results  []struct {
+		Name string `json:"name"`
+		URL  string `json:"url"`
+	} `json:"results"`
 }
 
-func commandExit() error {
+type config struct {
+	Next     string
+	Previous any
+}
+
+var supportedCommands map[string]cliCommand
+
+func commandExit(cfg *config) error {
 	fmt.Println("Closing the Pokedex... Goodbye!")
 	os.Exit(0)
 	return nil
 }
 
-func help() error {
+func help(cfg *config) error {
 	fmt.Println("Welcome to the Pokedex!")
-	fmt.Println("Usage:\n")
+	fmt.Print("Usage:\n\n")
 	for k, v := range supportedCommands {
 		fmt.Printf("%s: %s\n", k, v.description)
 	}
 	return nil
 }
 
+func getMap(cfg *config) error {
+	res, err := http.Get(cfg.Next)
+	if err != nil {
+		log.Fatal(err)
+	}
+	body, err := io.ReadAll(res.Body)
+	res.Body.Close()
+	if res.StatusCode > 299 {
+		log.Fatalf("Response failed with status code: %d and \n body: %s\n", res.StatusCode, body)
+	}
+	if err != nil {
+		log.Fatal(err)
+	}
+	currentLocationAreaList := locationAreaList{}
+	err = json.Unmarshal(body, &currentLocationAreaList)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, resultStruct := range currentLocationAreaList.Results {
+		fmt.Println(resultStruct.Name)
+	}
+	cfg.Next = currentLocationAreaList.Next
+	cfg.Previous = currentLocationAreaList.Previous
+	return nil
+}
+
+func init() {
+	supportedCommands = map[string]cliCommand{
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+		"help": {
+			name:        "help",
+			description: "Display the help menu",
+			callback:    help,
+		},
+		"map": {
+			name:        "map",
+			description: "Display Pokemon Areas",
+			callback:    getMap,
+		},
+	}
+}
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
+	cfg := config{}
+	cfg.Next = "https://pokeapi.co/api/v2/location-area/"
 	for {
 		fmt.Print("Pokedex > ")
 		scanner.Scan()
@@ -50,7 +105,7 @@ func main() {
 		userStringSlice := cleanInput(userString)
 		_, ok := supportedCommands[userStringSlice[0]]
 		if ok {
-			supportedCommands[userStringSlice[0]].callback()
+			supportedCommands[userStringSlice[0]].callback(&cfg)
 		} else {
 			fmt.Println("Unknown command")
 		}
---
+++ notes.md
@@ -0,0 +1,3 @@
+for map command:
+can set limit as query parameter - https://pokeapi.co/api/v2/location-area/?offset=20&limit=20
+and offset to move forward
